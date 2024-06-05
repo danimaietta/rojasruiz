0cc5c0b9e011a74b58328ecb1533b76d145ce386
danimaietta <danimaietta1@gmail.com>
2024-06-05 01:43:06 -0600

    sass added, need to fix everyfile

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,4 +1,4 @@
-import styles from './page.module.css'
+import styles from './page.module.scss'
 import React, { useState, useRef } from 'react'
 import { IoIosMail } from "react-icons/io";
 import constants from '@/constants/constants'
