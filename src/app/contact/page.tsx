5335cc11343b6ca82596e7c73cb996f38bd812dc
danimaietta <danimaietta1@gmail.com>
2024-06-07 16:50:06 -0600

    useReducer complete, next is add look and feel to the form

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,67 +3,64 @@ import React, { useState, useRef, useReducer } from 'react'
 import { IoIosMail } from "react-icons/io";
 import constants from '@/constants/constants'
 
+interface State {
+  isName: boolean,
+  isMail: boolean,
+  isPhone: boolean,
+  dropdownTitle: string
+}
+
+interface Action {
+  type: string, 
+  inputs?: State, 
+  error?: string
+}
+
 export default function Contact() {
-  const [isName, setIsName] = useState<boolean>(false)
-  const [isMail, setIsMail] = useState<boolean>(false)
-  const [isPhone, setIsPhone] = useState<boolean>(false)
   const [displaySuccess, setDisplaySuccess] = useState<boolean>(false)
-  const [dropdownTitle, setDropdownTitle] = useState<string>('¿Qué servicio necesita?')
   const dropDownClicked = useRef(false)
   const { contactDropdownOptions, maxCharacters, regexString, regexNumber, regexMail } = constants
 
   const checkInput = (type: string) => (e: React.ChangeEvent<HTMLInputElement>)  => {
     const { value } = e.target
-    !value.match(regexString) && type === 'string' ? setIsName(true) : setIsName(false)
-    !value.match(regexNumber) && type === 'number' ? setIsPhone(true) : setIsPhone(false)
-    !value.match(regexMail) && type === 'mail' ? setIsMail(true) : setIsMail(false)
+    !value.match(regexString) && type === 'string' ? dispatch({ type: 'name-success' }): dispatch({ type: 'name-denied' })
+    !value.match(regexNumber) && type === 'number' ? dispatch({ type: 'phone-success' }) : dispatch({ type: 'phone-denied' })
+    !value.match(regexMail) && type === 'mail' ? dispatch({ type: 'mail-success' }) : dispatch({ type: 'mail-denied' })
   }
 
   const sendEmail = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    if(!isName && !isMail && !isPhone){
+    if(!inputs.isName && !inputs.isMail && !inputs.isPhone){
       setDisplaySuccess(true)
     }
   }
 
   const changeDropdownTitle = (option: string) => {
     dropDownClicked.current = true
-    setDropdownTitle(option)
+    dispatch({ type: 'select-dropdown', inputs: { ...inputs, dropdownTitle: option } })
   }
 
   // useReducer
 
-  interface State {
-    isName: boolean,
-    isMail: boolean,
-    isPhone: boolean,
-    dropdownTitle: string
-  }
-
-  interface Action {
-    type: string, 
-    post: State, 
-    error: string
-  }
-
   function reducer(state: State, action: Action): State {
     switch (action.type){
-      case 'name-denied':
-        return { ...state, isName: false }
-      case 'mail-denied':
-        return { ...state, isMail: false }
-      default: 
-        return { ...state }
+      case 'name-denied': return { ...state, isName: false }
+      case 'name-success': return { ...state, isName: true }
+      case 'mail-denied': return { ...state, isMail: false }
+      case 'mail-success': return { ...state, isMail: true }
+      case 'phone-denied': return { ...state, isPhone: false }
+      case 'phone-success': return { ...state, isPhone: true }
+      case 'select-dropdown': return { ...state, dropdownTitle: action?.inputs?.dropdownTitle || ''}
+      default: return { ...state }
     }
   }
 
-  const [posts, dispatch] = useReducer(reducer, {
+  const [inputs, dispatch] = useReducer(reducer, {
     isName: false,
     isMail: false,
     isPhone: false,
     dropdownTitle: '¿Qué servicio necesita?'
   })
-
 
   return (
     <>
@@ -73,16 +70,16 @@ export default function Contact() {
           a través de nuestro formulario y nosotros le atenderemos.
         </p>
         <input type='text' placeholder='Nombre' onChange={checkInput('string')}></input>
-        { isName && <p> * nombre solo puede contener letras </p> }
+        { inputs.isName && <p> * nombre solo puede contener letras </p> }
         <input type='text' placeholder='Correo' onChange={checkInput('mail')}></input>
-        { isMail && <p> * correo no válido </p> }
+        { inputs.isMail && <p> * correo no válido </p> }
         <input type='text' placeholder='Teléfono' onChange={checkInput('number')}></input>
-        { isPhone && <p> * teléfono solo puede contener números </p> }
+        { inputs.isPhone && <p> * teléfono solo puede contener números </p> }
         <li className={styles['contact-dropdown']}>
           <p 
             className={dropDownClicked.current ? styles['dropdown-selected'] : styles['dropdown-placeholder']}
           > 
-            { dropdownTitle } 
+            { inputs.dropdownTitle } 
           </p>
           <ul className={styles['dropdown-items']}>
             {
